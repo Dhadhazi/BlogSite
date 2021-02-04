309e292ad01982bbca03713823dd9883f86cc7a3
Dhadhazi <dhadhazi@gmail.com>
2021-02-04 13:49:19 +0100

    page list to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,36 @@
 import * as React from "react";
-import Layout from "../components/Layout";
-import Seo from "../components/Seo";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { graphql, Link } from "gatsby";
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
-      <Seo title="Home Page" />
-      <div>Hello World</div>
+      <SEO title={`Home Page`} />
+      {posts.map((post) => {
+        return (
+          <Link to={post.slug} key={post.slug}>
+            <h2>{post.frontmatter.title}</h2>
+          </Link>
+        );
+      })}
     </Layout>
   );
 };
+
+export const pageQuery = graphql`
+  query {
+    allMdx(sort: { fields: [frontmatter___title], order: ASC }) {
+      nodes {
+        slug
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
